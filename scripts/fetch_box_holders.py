f14b445167cb61fcfe71018941595e57337d75da
cesarsld <jaimes.cesar88@gmail.com>
2021-03-06 17:47:29 +0100

    script

--- scripts/fetch_box_holders.py
+++ scripts/fetch_box_holders.py
@@ -1,15 +1,17 @@
 from brownie import NFTBoxesBox, web3
 import csv
+import numpy
 
 def main():
-    boxId = 1
-    seedStr = '0xa1386e40d957e239f054a0c0da0ba2eb2f92f88ac108e163bfe68d70ed3d35e3'
+    boxId = 2
+    seedStr = '0x4f22a72b37e5acea8fc863522419e05e71bc9947bd0559995ee58702533bbb44'
     byteSeed = web3.toBytes(hexstr=seedStr)
     seed  = web3.soliditySha3(['bytes32'], [byteSeed])
     seedInt = web3.toInt(seed)
-    box = NFTBoxesBox.at('0x5f8061f9d6a2bb4688f46491cca7658e214e2cb6')
+    box = NFTBoxesBox.at('0x067ab2FbdBED63401aF802d1DD786E6D83b0ff1B')
     print(f'Box contract at {box.address}\nFetching holders of box edition {boxId}...')
-    ids = 10
+    ids = 6
+    # event BoxBought(uint256 indexed boxMould, uint256 boxEdition, uint256 tokenId);
     box_contract = web3.eth.contract(address=box.address, abi=box.abi)
     filt = box_contract.events.BoxBought.createFilter(fromBlock=0, toBlock= 'latest', argument_filters={'boxMould':boxId})
     res = filt.get_all_entries()
@@ -31,8 +33,8 @@ def main():
         tempWinnerArray.clear()
     print(f'Distribution finished, writing data onto boxHolders_{boxId}.csv...')
     wtr = csv.writer(open (f'boxHolders_{boxId}.csv', 'w'), delimiter=',', lineterminator='\n')
-    for x in winnerArray:
-        wtr.writerow (x)
+    print(f'winner array length is {len(winnerArray)}')
+    wtr.writerows(winnerArray)
     print('done')
 
 def newSeed(seed, web3):
