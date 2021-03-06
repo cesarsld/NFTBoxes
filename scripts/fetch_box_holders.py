5f266f57e5a5dd3d34a3eb0e1d5d62e211d46ae1
cesarsld <jaimes.cesar88@gmail.com>
2021-03-06 18:21:19 +0100

    fix csv

--- scripts/fetch_box_holders.py
+++ scripts/fetch_box_holders.py
@@ -1,6 +1,5 @@
 from brownie import NFTBoxesBox, web3
 import csv
-import numpy
 
 def main():
     boxId = 2
@@ -11,7 +10,6 @@ def main():
     box = NFTBoxesBox.at('0x067ab2FbdBED63401aF802d1DD786E6D83b0ff1B')
     print(f'Box contract at {box.address}\nFetching holders of box edition {boxId}...')
     ids = 6
-    # event BoxBought(uint256 indexed boxMould, uint256 boxEdition, uint256 tokenId);
     box_contract = web3.eth.contract(address=box.address, abi=box.abi)
     filt = box_contract.events.BoxBought.createFilter(fromBlock=0, toBlock= 'latest', argument_filters={'boxMould':boxId})
     res = filt.get_all_entries()
@@ -32,9 +30,16 @@ def main():
         winnerArray.append([w for w in tempWinnerArray])
         tempWinnerArray.clear()
     print(f'Distribution finished, writing data onto boxHolders_{boxId}.csv...')
-    wtr = csv.writer(open (f'boxHolders_{boxId}.csv', 'w'), delimiter=',', lineterminator='\n')
-    print(f'winner array length is {len(winnerArray)}')
-    wtr.writerows(winnerArray)
+    strcsv = ''
+    for row in winnerArray:
+        for i in range(len(row)):
+            strcsv += row[i]
+            if i != len(row) - 1:
+                strcsv += ','
+        strcsv += '\n'
+    file = open (f'boxHolders_{boxId}.csv', 'w')
+    file.write(strcsv)
+    file.close()
     print('done')
 
 def newSeed(seed, web3):
